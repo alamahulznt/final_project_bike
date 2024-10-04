86b14b4a39fa91dc693eb6eabb0987b5651ffc60
alamahulznt <m180b4kx0311@bangkit.academy>
2024-10-04 17:33:15 +0700

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -126,7 +126,7 @@ min_date = pd.to_datetime(day_df['dateday']).dt.date.min()
 max_date = pd.to_datetime(day_df['dateday']).dt.date.max()
 
 with st.sidebar:
-    st.image('')
+    st.image('Dashboard/download.jpeg')
     # Mengambil start_date & end_date dari date_input
     start_date, end_date = st.date_input(
         label='Rentang Waktu',
